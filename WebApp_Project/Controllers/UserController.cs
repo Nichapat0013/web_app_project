b81a5091c1d84afc4c2213562a2a91761860aad4
Nichapat0013 <64010267@kmitl.ac.th>
2023-12-01 20:03:48 +0700

    add mvc project

---
+++ WebApp_Project/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using WebApp_Project.Data;
+using WebApp_Project.Filters;
+using WebApp_Project.Models;
+using WebApp_Project.Models.Forms;
+
+namespace WebApp_Project.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public UserController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUser(User obj)
+        {
+
+            try
+            {
+                var user = _db.Users.Where(user => user.UserName == obj.UserName).First();
+
+                if (obj.Password == obj.PasswordConfirmed )
+                {
+                    if (user.UserName != obj.UserName)
+                    {
+                    _db.Users.Add(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return Content("duplicate user");
+                   
+                    }
+                    
+                }
+                else
+                {
+                    Content("password wrong");
+                        return Content("password wrong");
+                }
+            }
+            catch (Exception)
+            {
+                _db.Users.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                /*Console.WriteLine("User not found.");
+                return View();*/
+            }
+        }
+
+
+
+        /*_db.Users.Add(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");*/
+
+
+        /*public IActionResult Profile()
+        {
+            IEnumerable<User> allUser = _db.Users;
+            return View(allUser);
+        }*/
+
+
+        [AuthorizeSession]
+        public IActionResult Profile()
+        {
+            var currentUser = HttpContext.Session.GetString("_CurrentUser");
+            var profile = _db.Users.Where(user => user.UserName == currentUser).FirstOrDefault();
+            ViewData["profile"] = profile;
+            return View();
+        }
+        public IActionResult Sign_in() 
+        { 
+            return View("~/Views/Home/Index.cshtml"); }
+        }
+}
---
+++ WebApp_Project/Models/Details/OrderDetail.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace WebApp_Project.Models.Details
+{
+    public class OrderDetail
+    {
+
+        public OrderDetail(Order order, User rider)
+        {
+            this.Id = order.Id;
+            this.Place = order.Place;
+            this.Restaurant = order.Restaurant;
+            this.Price = order.Price;
+            this.Menu = order.Menu;
+            this.Rider = rider;
+        }
+
+        public OrderDetail(Order order, User rider, User buyer)
+        {
+            this.Id = order.Id;
+            this.Place = order.Place;
+            this.Restaurant = order.Restaurant;
+            this.Price = order.Price;
+            this.Menu = order.Menu;
+            this.Rider = rider;
+            this.Buyer = buyer;
+        }
+
+        public int Id { get; set; }
+
+        public User Rider { get; set; }
+
+        public User Buyer { get; set; }
+
+        public string Place { get; set; }
+
+        public string Restaurant { get; set; }
+
+        public String? Menu { get; set; }
+
+        public String? Price { get; set; }
+    }
+
+
+    /*   public OrderDetail(Order order, User rider)
+       {
+           this.Id = order.Id;
+           this.Place = order.Place;
+           this.Restaurant = order.Restaurant;
+           this.Price = order.Price;
+           this.Menu = order.Menu;
+           this.Rider = rider;
+       }
+
+       public int Id { get; set; }
+
+       public User Rider { get; set; }
+
+       public User Buyer { get; set; }
+
+       public string Place { get; set; }
+
+       public string Restaurant { get; set; }
+
+       public String? Menu { get; set; }
+
+       public String? Price { get; set; }
+   }
+*/
+
+}
